8209c86fd9357e76a7dd34ccffe46e66d522059b
Danila <dakfadeev@gmail.com>
2022-08-30 23:23:38 +0300

    deploy

--- Bot_admin.py
+++ Bot_admin.py
@@ -3,14 +3,8 @@ from discord.ext import commands
 import os
 import time
 
-string1 = 'nNНнHh'
-string2 = '!Ii|иИeEеЕе́'
-string3 = 'МмMm'
-string4 = 'еЕэЭeEе́'
 BOT_PREFIX = '/'
-bot = commands.Bot(command_prefix=BOT_PREFIX)
-ban_msg = ["2д", "2D", "2d", "2Д", ":two::regional_indicator_d:", "2 д", "2 D", "2 d", "2 Д", "китайские мультики", "японские мультики",
-           "бурятские мультики", "Китайские мультики", "Японские мультики", "Бурятские мультики"]
+bot = commands.Bot(command_prefix=BOT_PREFIX, intents=discord.Intents.default())
 
 
 @bot.event
@@ -18,11 +12,6 @@ async def on_ready():
     print("Logged in as: " + bot.user.name + "\n")
     game = discord.Game("Симулятор бана")
     await bot.change_presence(activity=game)
-
-
-@bot.event
-async def on_message(msg):
-    pass
 
 
 b_token = os.environ.get('Token')
---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8
+
+ENV Token=
+
+ADD requirements.txt /requirements.txt
+ADD Bot_admin.py /main.py
+ADD okteto-stack.yaml /okteto-stack.yaml
+RUN pip install -r requirements.txt
+
+CMD ["python3", "main.py"]
