0a75f494b742d8aefcf17196cfc7088edb30c321
Пожилой молодой человек <49003989+Delivery-Klad@users.noreply.github.com>
2023-01-06 01:26:01 +0300

    Update Bot_admin.py

--- Bot_admin.py
+++ Bot_admin.py
@@ -1,12 +1,19 @@
 import os
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 from webserver import keep_alive
 
-BOT_PREFIX = '/'
-bot = commands.Bot(command_prefix=BOT_PREFIX, intents=discord.Intents.all())
+log_cannel_id = 855557026712780826
+BOT_PREFIX = '!'
+bot = commands.Bot(command_prefix=BOT_PREFIX,
+                   intents=discord.Intents.all())
+
+
+def channel():
+    return bot.get_channel(log_cannel_id)
 
 
 @bot.event
@@ -14,11 +21,17 @@ async def on_ready():
     print("Logged in as: " + bot.user.name + "\n")
     game = discord.Game("Симулятор бана")
     await bot.change_presence(activity=game)
+    try:
+        synced = await bot.tree.sync()
+        await channel().send(f"Logged in as: {bot.user.name}\nSynced {len(synced)} commands")
+    except Exception as e:
+        await channel().send(f"Logged in as: {bot.user.name}")
 
 
-@bot.event
-async def on_message(ctx):
-    print(ctx.content)
+@bot.tree.command(name="test")
+@app_commands.describe(test="test")
+async def test(interaction: discord.Interaction, test: str):
+    await interaction.response.send_message("test")
 
 
 keep_alive()
