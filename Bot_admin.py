7f861205808cc9f04f0e9a6ca9a73411242fbc78
Пожилой молодой человек <49003989+Delivery-Klad@users.noreply.github.com>
2023-03-25 22:18:41 +0300

    Update Bot_admin.py

--- Bot_admin.py
+++ Bot_admin.py
@@ -6,14 +6,71 @@ from discord.ext import commands
 
 from webserver import keep_alive
 
-log_cannel_id = 855557026712780826
-BOT_PREFIX = '!'
-bot = commands.Bot(command_prefix=BOT_PREFIX,
-                   intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+log_cannel = None
+
+alphabet = {
+    "a": "\U0001F1E6",
+    "b": "\U0001F1E7",
+    "c": "\U0001F1E8",
+    "d": "\U0001F1E9",
+    "e": "\U0001F1EA",
+    "f": "\U0001F1EB",
+    "g": "\U0001F1EC",
+    "h": "\U0001F1ED",
+    "i": "\U0001F1EE",
+    "j": "\U0001F1EF",
+    "k": "\U0001F1F0",
+    "l": "\U0001F1F1",
+    "m": "\U0001F1F2",
+    "n": "\U0001F1F3",
+    "o": "\U0001F1F4",
+    "p": "\U0001F1F5",
+    "q": "\U0001F1F6",
+    "r": "\U0001F1F7",
+    "s": "\U0001F1F8",
+    "t": "\U0001F1F9",
+    "u": "\U0001F1FA",
+    "v": "\U0001F1FB",
+    "w": "\U0001F1FC",
+    "x": "\U0001F1FD",
+    "y": "\U0001F1FE",
+    "z": "\U0001F1FF",
+    "space": "\U000025AA"
+}
+
+alphabet_variants = {
+    "a": "\U0001F170",
+    "b": "\U0001F171",
+    "o": "\U0001F17E",
+    "p": "\U0001F17F",
+    "m": "\U000024C2"
+}
+
+alphabet_addiotional = {
+    "ab": "\U0001F18E",
+    "cl": "\U0001F191",
+    "ok": "\U0001F197",
+    "sos": "\U0001F198",
+    "ng": "\U0001F196",
+    "vs": "\U0001F19A"
+}
 
 
-def channel():
-    return bot.get_channel(log_cannel_id)
+def preload():
+    global log_cannel, role_msg_id
+    log_cannel = bot.get_channel(855557026712780826)
+
+
+async def log(text, type="Info"):
+    if type == "Info":
+        await log_cannel.send(f'**{type}:** {text}')
+    else:
+        await log_cannel.send(f'> **{type}:** {text}')
+
+
+async def failed_use(interaction, text):
+    await log_cannel.send(f'> **Error:** {interaction.user} `failed` to use command (`{text}`)')
 
 
 @bot.event
