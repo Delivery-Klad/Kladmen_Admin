860e3ef0e278f8f459ad8a2b10115499b725cd96
Пожилой молодой человек <49003989+Delivery-Klad@users.noreply.github.com>
2023-04-03 09:21:48 +0300

    Update data.py

--- files/data.py
+++ files/data.py
@@ -1,1 +1,50 @@
-.
+alphabet = {
+    "a": "\U0001F1E6",
+    "b": "\U0001F1E7",
+    "c": "\U0001F1E8",
+    "d": "\U0001F1E9",
+    "e": "\U0001F1EA",
+    "f": "\U0001F1EB",
+    "g": "\U0001F1EC",
+    "h": "\U0001F1ED",
+    "i": "\U0001F1EE",
+    "j": "\U0001F1EF",
+    "k": "\U0001F1F0",
+    "l": "\U0001F1F1",
+    "m": "\U0001F1F2",
+    "n": "\U0001F1F3",
+    "o": "\U0001F1F4",
+    "p": "\U0001F1F5",
+    "q": "\U0001F1F6",
+    "r": "\U0001F1F7",
+    "s": "\U0001F1F8",
+    "t": "\U0001F1F9",
+    "u": "\U0001F1FA",
+    "v": "\U0001F1FB",
+    "w": "\U0001F1FC",
+    "x": "\U0001F1FD",
+    "y": "\U0001F1FE",
+    "z": "\U0001F1FF",
+    "space": "\U000025AA"
+}
+
+alphabet_variants = {
+    "a": "\U0001F170",
+    "b": "\U0001F171",
+    "o": "\U0001F17E",
+    "p": "\U0001F17F",
+    "m": "\U000024C2"
+}
+
+alphabet_addiotional = {
+    "ab": "\U0001F18E",
+    "cl": "\U0001F191",
+    "ok": "\U0001F197",
+    "sos": "\U0001F198",
+    "ng": "\U0001F196",
+    "vs": "\U0001F19A"
+}
+
+ban_tags = ["child", "children", "kid", "girl", "girls", "cartoon", "artwork", "fashion", "art", "cute", "comic", "comics", "identity", "dress", "sexy", "doll", "fantasy", "pretty", "adult", "glamor", "animated"]
+
+stop_tags = ["human", "person", "portrait", "cover girl", "20s"]
